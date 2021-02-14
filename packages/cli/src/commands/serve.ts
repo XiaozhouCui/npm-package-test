211a021e60ab4f6ea216c9dd24633613b95de90c
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-14 11:11:55 +1000

    Connecting cli and local-api package

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,8 +1,13 @@
 import { Command } from "commander";
+import { serve } from "local-api";
 
 export const serveCommand = new Command()
-  .command("serve")
+  // [filename] is an optional value
+  .command("serve [filename]")
   .description("Open a file for editing")
-  .action(() => {
-    console.log("Getting ready to serve a file");
+  // <number> is a required value, 4005 is default value
+  .option("-p, --port <number>", "port to run server on", "4005")
+  .action((filename = "notebook.js", options: { port: string }) => {
+    // calling local-api package
+    serve(parseInt(options.port), filename, "/");
   });
