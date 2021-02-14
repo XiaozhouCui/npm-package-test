ed9299b74a91701e7831a05007ad09974474de0d
Joe Cui <xiaozhou.cui@gmail.com>
2021-02-14 22:01:29 +1000

    Adding dev mode and prod mode for express

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,8 +1,30 @@
+import path from "path";
 import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
-export const serve = (port: number, filename: string, dir: string) => {
+export const serve = (
+  port: number,
+  filename: string,
+  dir: string,
+  useProxy: boolean
+) => {
   // setup express server
   const app = express();
+
+  if (useProxy) {
+    // Dev mode: CRA dev server will also be available at port "http://localhost:4005"
+    app.use(
+      createProxyMiddleware({
+        target: "http://localhost:3000",
+        ws: true, // web socket support
+        logLevel: "silent",
+      })
+    );
+  } else {
+    // Prod mode (CLI installed): serve the built files of react app inside node_modules dependency
+    const packagePath = require.resolve("local-client/build/index.html");
+    app.use(express.static(path.dirname(packagePath)));
+  }
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on("error", reject);
